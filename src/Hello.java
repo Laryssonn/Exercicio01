8a2a82a250a4ace1f9abda7a3a8f933c2989433d
Larysson da Silva Nascimento <data52691@datainfo.inf.br>
2018-06-12 11:59:02 -0300

    Exercicios1 sobre numeros

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,48 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		exibirNumerosPrimos();
+	}
+
+	public static void par(int x) {
+		System.out.println(x + " é par");
+	}
+
+	public static void impar(int x) {
+		if (x % 2 == 1) {
+			System.out.println(x + " é impar");
+		}
+
+	}
+	public static void exibirNumerosParesImpares(){
+		for (int x = 1; x <= 100; x++) {
+			if (x % 2 == 0) {
+				par(x);
+			} else {
+				impar(x);
+			}
+
+		}
+	}
+	public static void exibirNumerosPrimos(){
+		int c =0;
+		for(int x = 0;x <= 100;x++){
+			int aux = x;
+        	while(aux > 0){
+           	 if(x % aux ==0 ){
+           		 c++;
+           	 }
+           	
+           	 aux--;
+             }
+        	 if(c == 2){
+        		System.out.println(x);
+        	    c=0;
+        	    
+       	     }else{
+       	    	 c=0;
+       	     }
+		}
 	}
 
 }
